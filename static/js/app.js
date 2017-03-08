b264147598518e2a98640904849ed0614c50e8ff
Justin Harwood <harwoodjp@g.cofc.edu>
2017-03-08 18:45:34 -0500

    message sidebar

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,22 @@
 function toggleContact() {
-	const contactBox = document.querySelector(".services__service--contact");
+	const contactBox = document.querySelector(".services__service--contact"),
+		contactBottom = document.querySelector(".contact__message"),
+		messageForm = document.querySelector(".message-form"),
+		messageFormExit = document.querySelector(".message-form__exit");
+	
+	contactBottom.addEventListener("click", function() {
+		messageForm.classList.toggle("message-form--active");
+	});
+
 	contactBox.addEventListener("click", function() {
-		console.log("active chat");
-	})
+		messageForm.classList.toggle("message-form--active");
+	});
+	messageFormExit.addEventListener("click", function() {
+		console.log(messageForm);
+		messageForm.classList.toggle("message-form--active");
+	});
+
+
 }
 
 toggleContact();
